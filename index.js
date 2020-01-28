214392c9a79ffd2d438bd6b0569d920afbe5b1ad
rleee <danlee0150@gmail.com>
2020-01-28 13:08:38 +0800

    add socket to server and front end

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.render("index.ejs");
+});
+
+io.sockets.on("connection", socket => {
+  socket.on("username", username => {
+    socket.username = username;
+    io.emit("is_online", "🔵 <i>" + socket.username + " join the chat..</i>");
+  });
+
+  socket.on("disconnect", username => {
+    io.emit("is_online", "🔴 <i>" + socket.username + " left the chat..</i>");
+  });
+
+  socket.on("chat_message", message => {
+    io.emit(
+      "chat_message",
+      "<strong>" + socket.username + "</strong>: " + message
+    );
+  });
 });
 
 const server = http.listen(5000, () => {
